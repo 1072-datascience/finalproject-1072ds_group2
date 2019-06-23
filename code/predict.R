bb99f4c19b6a8992e2ec7f12044cdff55400d160
Einz <jason199786109@gmail.com>
2019-06-23 21:44:02 +0800

    Prediction.

---
+++ code/predict.R
@@ -0,0 +1,33 @@
+source("model.R")
+
+seed = 700
+set.seed(seed)
+
+# read parameters
+args = commandArgs(trailingOnly=TRUE)
+if (length(args)==0) {
+  stop("USAGE: Rscript predict.R --train ../data/features.csv --predict ../results/predict.csv", call.=FALSE)
+} else {
+  # Read arguments and store they in variables
+  target <- input <- output <- c()
+  for(arg in args){
+    if(arg == "--train" || arg == "--predict") {
+      curr_type <- arg
+    } else {
+      if(curr_type == "--train") train_path <- arg
+      else if(curr_type == "--predict") predict_path <- arg
+    }
+  }
+}
+
+# Load Data
+train_data <- read.csv(train_path, stringsAsFactors=F)
+
+row_count <- nrow(train_data)
+train_idx = 1:floor(row_count*0.7)
+train <- train_data[train_idx, ]
+test <- train_data[-train_idx, ]
+
+predict <- final_model(train, test)
+out_data <- cbind(predict, test)
+write.csv(out_data, predict_path, quote=F, row.names=F)
