837e9d71181e416d36bac76322f0442f36c7820e
Einz <jason199786109@gmail.com>
2019-06-23 18:56:39 +0800

    Minor Fixed.

--- code/model.R
+++ code/model.R
@@ -3,37 +3,46 @@ library(rpart)
 library(caret)
 
 
-model1_loss <- function(train)
+model1_loss <- function(train, test)
 {
   model <- lm(target~., data=train)
   
   train_pred <- predict(model, train)
   train_acc <- mean((train_pred - train$target) ^2) ^0.5
   
-  return(train_acc)
+  test_pred <- predict(model, test)
+  test_acc <- mean((test_pred - test$target) ^2) ^0.5
+  
+  return(c(train_acc, test_acc))
 }
 
-model2_loss <- function(train)
+model2_loss <- function(train, test)
 {
   model <- glm(target~., data=train)
 
   train_pred <- predict(model, train)
   train_acc <- mean((train_pred - train$target) ^2) ^0.5
 
-  return(train_acc)
+  test_pred <- predict(model, test)
+  test_acc <- mean((test_pred - test$target) ^2) ^0.5
+  
+  return(c(train_acc, test_acc))
 }
 
-model3_loss <- function(train)
+model3_loss <- function(train, test)
 {
   model <- rpart(target~., method="anova", data=train)
 
   train_pred <- predict(model, train)
   train_acc <- mean((train_pred - train$target) ^2) ^0.5
 
-  return(train_acc)
+  test_pred <- predict(model, test)
+  test_acc <- mean((test_pred - test$target) ^2) ^0.5
+  
+  return(c(train_acc, test_acc))
 }
 
-model4_loss <- function(train)
+model4_loss <- function(train, test)
 {
   fitControl <- trainControl(method="repeatedcv", number=2, repeats=1)
   model <- train(target~., data=train, method="gbm", trControl=fitControl, verbose=FALSE)
@@ -41,5 +50,8 @@ model4_loss <- function(train)
   train_pred <- predict(model, train)
   train_acc <- mean((train_pred - train$target) ^2) ^0.5
   
-  return(train_acc)
+  test_pred <- predict(model, test)
+  test_acc <- mean((test_pred - test$target) ^2) ^0.5
+  
+  return(c(train_acc, test_acc))
 }
