f7f334733616a264cac4404e029eaa8310dcacea
ray0129 <dude2214@gmail.com>
2019-06-23 19:38:38 +0800

    revise k-fold

--- main.R
+++ main.R
@@ -36,25 +36,64 @@ train_data <- train_data[sample(1:row_count), ]
 #Random number list of k-fold
 cvlist <- CVgroup(k = fold,datasize = row_count,seed = seed)
 
-model1_result = c()
-model2_result = c()
-model3_result = c()
-model4_result = c()
+model1_train_result <- c()
+model1_validation_result <- c()
+model1_test_result <- c()
+
+model2_train_result <- c()
+model2_validation_result <- c()
+model2_test_result <- c()
+
+model3_train_result <- c()
+model3_validation_result <- c()
+model3_test_result <- c()
+
+model4_train_result <- c()
+model4_validation_result <- c()
+model4_test_result <- c()
 
 #k-fold
 for(i in c(1:fold)){
-  temp <- as.numeric(as.character(unlist(cvlist[[i]]))) 
-  k_train_data <- train_data[temp,]
+  temp1 <- as.numeric(as.character(unlist(cvlist[[i]]))) 
+  temp2 <- as.numeric(as.character(unlist(cvlist[[((i%%fold)+1)]])))
+  temp3 <- c(temp1,temp2)
+  k_train_data <- train_data[-temp3,]
+  k_validation_data <- train_data[temp1,]
+  k_test_data <- train_data[temp2,]
   
-  model1_result <- c(model1_result, model1_loss(k_train_data))
-  model2_result <- c(model2_result, model2_loss(k_train_data))
-  model3_result <- c(model3_result, model3_loss(k_train_data))
-  model4_result <- c(model4_result, model4_loss(k_train_data))
+  model1_train_result <- c(model1_train_result, model1_loss(k_train_data,k_train_data))
+  model1_validation_result <- c(model1_validation_result, model1_loss(k_train_data,k_validation_data))
+  model1_test_result <- c(model1_test_result, model1_loss(k_train_data,k_test_data))
+  
+  model2_train_result <- c(model2_train_result, model2_loss(k_train_data,k_train_data))
+  model2_validation_result <- c(model2_validation_result, model2_loss(k_train_data,k_validation_data))
+  model2_test_result <- c(model2_test_result, model2_loss(k_train_data,k_test_data))
+  
+  model3_train_result <- c(model3_train_result, model3_loss(k_train_data,k_train_data))
+  model3_validation_result <- c(model3_validation_result, model3_loss(k_train_data,k_validation_data))
+  model3_test_result <- c(model3_test_result, model3_loss(k_train_data,k_test_data))
+
+  model4_train_result <- c(model4_train_result, model4_loss(k_train_data,k_train_data))
+  model4_validation_result <- c(model4_validation_result, model4_loss(k_train_data,k_validation_data))
+  model4_test_result <- c(model4_test_result, model4_loss(k_train_data,k_test_data))
 }
 
-print(mean(model1_result))
-print(mean(model2_result))
-print(mean(model3_result))
-print(mean(model4_result))
+print("model1")
+print(mean(model1_train_result))
+print(mean(model1_validation_result))
+print(mean(model1_test_result))
 
+print("model2")
+print(mean(model2_train_result))
+print(mean(model2_validation_result))
+print(mean(model2_test_result))
 
+print("model3")
+print(mean(model3_train_result))
+print(mean(model3_validation_result))
+print(mean(model3_test_result))
+
+print("model4")
+print(mean(model4_train_result))
+print(mean(model4_validation_result))
+print(mean(model4_test_result))
